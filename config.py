80b3c00f9af30be652926c131329508e21d2d57b
Olakunle Akinpelu <ooa3603@gmail.com>
2021-04-13 14:39:48 -0400

    initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = '/Users/Kunle/Google Drive/_Software Engineering/Project/C Stage/Akinpelu-Olakunle-C-Stage/Riki/content'
+USER_DIR = '/Users/Kunle/Google Drive/_Software Engineering/Project/C Stage/Akinpelu-Olakunle-C-Stage/Riki/user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
